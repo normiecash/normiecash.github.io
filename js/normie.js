47b640b6d88d86a8722b7ba1764f3240fc446bff
normiecash <normies4cash@gmail.com>
2017-09-18 09:54:02 -0700

    adding stats to page

--- js/normie.js
+++ js/normie.js
@@ -83,3 +83,70 @@ $('[data-toggle="tooltip"]').on('shown.bs.tooltip', function () {
         $('[data-toggle="tooltip"]').tooltip('hide');
     }, 1000);
 })
+
+console.log("nothing to see here, move along");
+
+function decodeStats(response, price) {
+    if (response == null) return null;
+
+    var result = response.result;
+    if (result == null || result.length == null || result.length < 193) return null;
+
+    var weiPerEther = new BigNumber("1000000000000000000", 10);
+
+    var totalContributionExact = new BigNumber(result.substr(2, 64), 16).div(weiPerEther);
+    var totalContributionUSDExact = totalContributionExact.times(new BigNumber(price));
+
+    return {
+        totalContribution: totalContributionExact.round(3, BigNumber.ROUND_DOWN),
+        totalContributionUSD: totalContributionUSDExact.round(0, BigNumber.ROUND_DOWN),
+        totalContributionTVs: totalContributionUSDExact.div(new BigNumber("1200")).round(0, BigNumber.ROUND_DOWN),
+        totalSupply: new BigNumber(result.substr(66, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        totalBonusTokensIssued: new BigNumber(result.substr(130, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        purchasingAllowed: new BigNumber(result.substr(194, 64), 16).isZero() == false
+    };
+}
+
+function getStats(price) {
+    var url = "https://api.etherscan.io/api?module=proxy&action=eth_call&to=0xbc7406c26a2ed08267d6daa298f0909752e264f4&data=0xc59d48470000000000000000000000000000000000000000000000000000000000000000&tag=latest";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) { return decodeStats(data, price); });
+}
+
+function getPrice() {
+    var url = "https://api.etherscan.io/api?module=stats&action=ethprice";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) {
+        if (data == null) return null;
+        if (data.result == null) return null;
+        if (data.result.ethusd == null) return null;
+
+        return parseFloat(data.result.ethusd);
+    });
+}
+
+function updatePage(stats) {
+    if (stats == null) return;
+
+
+    $("#total-ether").text(stats.totalContribution.toFixed(3));
+
+    $("#total-usd").text("$" + stats.totalContributionUSD.toFixed(0));
+
+    $("#total-tokens").text(stats.totalSupply.toFixed(3));
+
+    $("#stats").show();
+}
+
+function refresh() { getPrice().then(getStats).then(updatePage); }
+
+$(function() {
+    try {
+        refresh();
+        setInterval(refresh, 1000 * 60 * 5);
+    } catch (err) { }
+});
